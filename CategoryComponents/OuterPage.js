58e04ba85a710e99607d568d2d889b757eb663b5
raeunlee <raeun19@naver.com>
2021-11-04 16:09:59 +0900

    pull

--- CategoryComponents/OuterPage.js
+++ CategoryComponents/OuterPage.js
@@ -3,12 +3,14 @@ import { useNavigation } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import React, { useCallback } from 'react';
 import { StyleSheet, Text, View, Image } from "react-native";
+import { useState, useContext, useEffect,} from 'react'
 import { firestore } from "../utils/firebase";
 import * as Font from 'expo-font';
 import { render } from 'react-dom';
 import { TouchableHighlight, TouchableOpacity } from "react-native-gesture-handler";
 import { ScrollView } from "react-native-gesture-handler";
 
+const databaseURL = "https://green-light-1030-default-rtdb.firebaseio.com";
 
 Font.loadAsync({
     Vitro_pride: require('../assets/fonts/Vitro_pride.ttf'),
@@ -23,40 +25,79 @@ Font.loadAsync({
 
 
 export default function CategoryOuter() {
+const navigation = useNavigation();
+const[outerName, setOuterName] = useState(0);
+const[outerPrice, setOuterPrice] = useState(0);
+const[outerImg, setOuterImg] = useState(0);
+const[outerStore, setOuterStore] = useState(0);
+
+const handleFireBase = async()=>{
+  const document = await firestore.collection("outer").doc("outer1").get();
+  const tempName = await document.get("name");
+  const tempPrice = await document.get("price");
+  const tempImg = await document.get("uri");
+  const tempStore = await document.get("store");
+
+  setOuterName(tempName);
+  setOuterPrice(tempPrice);
+  setOuterImg(tempImg);
+  setOuterStore(tempStore);
+}
+handleFireBase();
 
 const Main = () => {
     return(
       <View style={styles.main}>
-              
+
+        <View style={styles.header}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Image
+           source={require("../icon+image/back.png")}
+            style={styles.backIcon}/>
+
+        </TouchableOpacity>
+        <Text style={styles.headerText}>{outerName}</Text>
+         
+        </View>              
+
+
+
       <View style = {styles.box1}>
             <Image source= {require("../icon+image/outer_brownjacket1.png")} 
             style={styles.image} />
       </View>
       <View style={styles.box3}>
         <Text style = {styles.textSmall}>
-          프로이탁
+          {outerStore}
         </Text>
         <Text style = {styles.textBold}>
-          Brown Jacket
+          {outerName}
         </Text>
         <Text style = {styles.textSmall}>
-          45,000원
+          {outerPrice}
         </Text>
       </View>
       <View style = {styles.box2}>
           <View style = {styles.option}>
-          <Text style = {styles.textBold}>옵션선택칸</Text> 
+          <Text style = {styles.textBold}>Freesize</Text> 
           </View>
 
           <View style = {styles.payment}>
           <Image source ={require("../icon+image/purchase_button.png")}
           style={styles.purchase} />
-          </View>
+          </View> 
 
           <View style = {styles.share}>
           <Image source ={require("../icon+image/share.png")}
-          style={styles.share} /> 
+          style={styles.share_img} /> 
           </View>
+
+          <View style = {styles.like}>
+          <Image source ={require("../icon+image/outer_like.png")}
+          style={styles.like_img} /> 
+          </View>
+
+        
       </View>
 
 
@@ -107,7 +148,26 @@ const styles = StyleSheet.create({
     width:'100%',
 
   },
+  header:{
+    marginTop:"5%",
+    height:"5%",
+    flexDirection:"row",
+    alignItems:"center",
+  },
 
+  backIcon:{
+  width:25,
+  height:25,
+  marginLeft:"20%"
+  },
+
+  headerText:{
+    flex:1,
+    fontFamily:"BinggraeMelona-Bold",
+    color: "black",
+    fontSize:25,
+    marginLeft:"7%"
+  },
   main:{
     width:'100%',
     backgroundColor:'white',
@@ -127,7 +187,7 @@ const styles = StyleSheet.create({
     width:'90%',
     flexDirection: 'column',
     backgroundColor: '#b6b6b6',
-    marginTop: '10%',
+    marginTop: '3%',
     
   },
 
@@ -136,7 +196,6 @@ const styles = StyleSheet.create({
  //   height:'10%',
     flexDirection: 'row',
     backgroundColor: 'white',
-    marginBottom: '3%',
     //alignItems: "center",
   },
 
@@ -148,29 +207,35 @@ const styles = StyleSheet.create({
     marginBottom: '3%',
   },
   line1:{
+    marginTop:'-15%',
     width:'100%',
     height:'0.2%',
     backgroundColor: '#b6b6b6',
   },
   option:{
-    width:'30%',
+    width:'25%',
    // height:'100%',
     backgroundColor: 'white',
-    marginLeft: "10%",
+    marginTop:'2%',
+    marginLeft: "5%",
   },
   
   payment: {
-    width:'30%',
-  //  height:'100%',
+    width:'32%',
     backgroundColor: 'white',
-    marginLeft: "10%",
-    marginRight: "10%",
+    marginRight: "2%",
+    marginLeft:"5%",
+  }, 
+  like:{
+    width: '14%',
   },
   share:{
-    marginLeft: "10%",
-    width: '10%',
+    marginTop:"2%",
+    marginLeft: "4%",
+    width: '14%',
 
   },
+ 
   
   image: {
     width: '100%',
@@ -181,8 +246,11 @@ const styles = StyleSheet.create({
   purchase: {
     width: '100%',
   },
-  share:{
-    width:'25%',
+  like_img:{
+    width:'100%',
+  },
+  share_img:{
+    width:'50%',
   },
   textSmall:{
     fontSize:16,
@@ -190,7 +258,7 @@ const styles = StyleSheet.create({
     marginLeft:'5%',
     },
   textBold:{
-    fontSize:18,
+    fontSize:20,
     fontFamily: "BinggraeMelonaBold",
     marginLeft:'5%',
   }
